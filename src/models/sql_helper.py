648da4c071bcb9d3fa9408a5a7d8bca8bfd1bebc
Rushil Patel <you@example.com>
2023-11-16 16:43:51 -0500

    Updated Email code so it sends email to currently logged in user

--- src/models/sql_helper.py
+++ src/models/sql_helper.py
@@ -1,5 +1,7 @@
 import pymysql
 import os
+
+
 class sql_helper:
     def __init__(self):
         self.connection_obj = None
@@ -7,34 +9,35 @@ class sql_helper:
     def connect_database(self):
         try:
             self.connection_obj = pymysql.connect(
-                    host='localhost',
-                    port = 3306,
-                    user = 'root',
-                    password = '1234',
-                    db = "simpli",
-                    autocommit=True
-                    ) 
+                host='localhost',
+                port=3306,
+                user='root',
+                password='1234',
+                db="simpli",
+                autocommit=True
+            )
         except:
             pass
-            #Need to import error handling class
+            # Need to import error handling class
 
     def disconnect_database(self):
         try:
             self.connection_obj.close()
         except:
             pass
-            #Need to import error handling class
-    
+            # Need to import error handling class
+
     def run_query(self, query):
         self.connect_database()
         tempCursor = self.connection_obj.cursor()
         tempCursor.execute(query)
-        output = tempCursor.fetchall() 
-        self.disconnect_database()   
+        output = tempCursor.fetchall()
+        self.disconnect_database()
         return output
 
-if __name__=='__main__':
+
+if __name__ == '__main__':
     sql = sql_helper()
-    output =sql.run_query("show tables")
-    print(output)
+    output = sql.run_query("show tables")
+    print("this is printed!", output)
     print("Hello")
--- src/models/task_model.py
+++ src/models/task_model.py
@@ -2,7 +2,8 @@ import pandas as pd
 from src.models.sql_helper import sql_helper
 from datetime import datetime, timedelta, date
 import uuid
-from flask import flash
+from flask import flash, Flask, session
+# from flask import Flask, render_template, redirect, session, request
 # Import smtplib for the actual sending function
 import smtplib
 
@@ -12,56 +13,62 @@ from email.mime.multipart import MIMEMultipart
 
 con = sql_helper()
 
+
 class task_model:
     def __init__(self):
         pass
 
     def get_all_tasks(username):
-        query = "SELECT *, Categories.Category_name, DATE(Startdate), DATE(Duedate) FROM Tasks JOIN Categories ON Tasks.Category= Categories.Category_ID WHERE UserID = " + f"'{username}'" + ";"
+        query = "SELECT *, Categories.Category_name, DATE(Startdate), DATE(Duedate) FROM Tasks JOIN Categories ON Tasks.Category= Categories.Category_ID WHERE UserID = " + \
+            f"'{username}'" + ";"
         print(query)
         result = con.run_query(query)
         result = pd.DataFrame(list(result))
         return result.to_dict('records')
-    
+
     def get_this_week_tasks(currUserName, current_date=None):
-        if(current_date == None):
+        if (current_date == None):
             current_date = date.today()
         dt = current_date
         start_date = dt - timedelta(days=dt.weekday())
         end_date = start_date + timedelta(days=6)
-        query = "SELECT *, Categories.Category_name FROM Tasks JOIN Categories ON Tasks.Category= Categories.Category_ID WHERE (Startdate <='"+str(end_date)+"' AND Duedate >='"+str(start_date)+'\' AND UserID = '+"\'"+currUserName+'\')'
+        query = "SELECT *, Categories.Category_name FROM Tasks JOIN Categories ON Tasks.Category= Categories.Category_ID WHERE (Startdate <='"+str(
+            end_date)+"' AND Duedate >='"+str(start_date)+'\' AND UserID = '+"\'"+currUserName+'\')'
         result = con.run_query(query)
         result = pd.DataFrame(list(result))
         return result.to_dict('records')
 
     def get_todays_tasks(currUserName, current_date=None):
-        if(current_date == None):
+        if (current_date == None):
             current_date = date.today()
         dt = current_date
         start_date = dt - timedelta(days=dt.weekday())
         end_date = start_date + timedelta(hours=23)
-        query = "SELECT *, Categories.Category_name FROM Tasks JOIN Categories ON Tasks.Category= Categories.Category_ID WHERE (Startdate <='"+str(end_date)+"' AND Duedate >='"+str(start_date)+'\' AND UserID = '+"\'"+currUserName+'\')'
+        query = "SELECT *, Categories.Category_name FROM Tasks JOIN Categories ON Tasks.Category= Categories.Category_ID WHERE (Startdate <='"+str(
+            end_date)+"' AND Duedate >='"+str(start_date)+'\' AND UserID = '+"\'"+currUserName+'\')'
         result = con.run_query(query)
         result = pd.DataFrame(list(result))
         return result.to_dict('records')
 
     def get_backlog(currUserName, current_date=None):
-        if(current_date == None):
+        if (current_date == None):
             current_date = date.today()
         dt = current_date
         start_date = dt - timedelta(days=dt.weekday())
-        query = "SELECT  *, Categories.Category_name, DATE(Duedate) FROM Tasks JOIN Categories ON Tasks.Category= Categories.Category_ID WHERE Duedate <='"+str(start_date)+'\' and status <> "Done" AND UserID = '+"\'"+currUserName+"\'"
+        query = "SELECT  *, Categories.Category_name, DATE(Duedate) FROM Tasks JOIN Categories ON Tasks.Category= Categories.Category_ID WHERE Duedate <='"+str(
+            start_date)+'\' and status <> "Done" AND UserID = '+"\'"+currUserName+"\'"
         result = con.run_query(query)
         result = pd.DataFrame(list(result))
         return result.to_dict('records')
 
     def get_future_tasks(currUserName, current_date=None):
-        if(current_date == None):
+        if (current_date == None):
             current_date = date.today()
         dt = current_date
         start_date = dt - timedelta(days=dt.weekday())
         end_date = start_date + timedelta(days=6)
-        query = "SELECT  *, Categories.Category_name, DATE(Duedate) FROM Tasks JOIN Categories ON Tasks.Category= Categories.Category_ID WHERE Startdate >='"+str(end_date)+"' AND UserID = \'"+currUserName+"\'"
+        query = "SELECT  *, Categories.Category_name, DATE(Duedate) FROM Tasks JOIN Categories ON Tasks.Category= Categories.Category_ID WHERE Startdate >='"+str(
+            end_date)+"' AND UserID = \'"+currUserName+"\'"
         result = con.run_query(query)
         result = pd.DataFrame(list(result))
         return result.to_dict('records')
@@ -74,11 +81,12 @@ class task_model:
             columns += str(key)+', '
             values += "'"+str(value)+"', "
 
-        query = "INSERT INTO Tasks ("+columns[:-2]+" ) VALUES (" + values[:-2]+" );"
+        query = "INSERT INTO Tasks (" + \
+            columns[:-2]+" ) VALUES (" + values[:-2]+" );"
         print(query)
         con.run_query(query)
         return
-    
+
     def create_subtasks(self, data):
         # print(data)
         # print(taskid)
@@ -90,15 +98,23 @@ class task_model:
             columns += str(key)+', '
             values += "'"+str(value)+"', "
 
-        query = "INSERT INTO Sub_tasks("+columns[:-2]+" ) VALUES (" + values[:-2]+" );"
+        query = "INSERT INTO Sub_tasks(" + \
+            columns[:-2]+" ) VALUES (" + values[:-2]+" );"
         print(query)
         con.run_query(query)
-        
+
         return
 
     def delete_task(self, taskid):
         querySub = f"SELECT * from Sub_tasks where TaskID = '{taskid}';"
         subTasks = con.run_query(querySub)
+        currUserName = session["username"]
+        user_email = con.run_query(
+            f"select EmailId from user where user.UserId = '{currUserName}';")
+        you = user_email[0][0]
+        print("THE CURRENT USER IS:", currUserName)
+        print("THE CURRENT USER EMIAL IS:", user_email[0][0])
+
         if len(subTasks) > 0:
             ids = ""
             for i in subTasks:
@@ -107,12 +123,12 @@ class task_model:
             ids = ids[:-2]
             queryDel = f"DELETE from Sub_tasks where STaskID IN ({ids});"
             con.run_query(queryDel)
-        query = "DELETE FROM Tasks WHERE Taskid ='"+ taskid+"';"
+        query = "DELETE FROM Tasks WHERE Taskid ='" + taskid+"';"
         con.run_query(query)
 
         # SOF EMAIL #
         me = 'nitinjain0455@gmail.com'
-        you = 'muskyk32@gmail.com'
+        # you = 'muskyk32@gmail.com'
         msg = MIMEMultipart('alternative')
         msg['Subject'] = 'test file'
         msg['From'] = me
@@ -149,7 +165,7 @@ class task_model:
         mail.quit()
         return
     # EOF #
-        
+
     def get_task_by_id(self, taskid):
         query = "SELECT * FROM tasks WHERE Taskid =" + taskid
         result = con.run_query(query)
@@ -174,4 +190,3 @@ class task_model:
         result = con.run_query(query)
         result = pd.DataFrame(list(result))
         return result.to_dict('records')
-    
