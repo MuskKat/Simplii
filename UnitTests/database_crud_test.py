1691f316102c5427f76fd4f58824ece5fcfb8d86
Muskan Katoch <135174176+MuskKat@users.noreply.github.com>
2023-11-26 11:53:41 -0500

    Update database_crud_test.py

--- UnitTests/database_crud_test.py
+++ UnitTests/database_crud_test.py
@@ -23,13 +23,13 @@ class Database_Table_Test(unittest.TestCase):
         addTask= sqlObj.run_query('Insert into Tasks Values ( 1000,1, "Get milk and bread", "Waiting", "2021-11-02", "2021-11-06", 4, 1, "xyz")')
         task_details = sqlObj.run_query('select * from Tasks where Taskname = "Get milk and bread"')
         task_details = str(task_details)
-        self.assertTrue('1000' in task_details)
+        self.assertTrue('Get milk and bread' in task_details)
         
     #Check if the delete operation with AWS RDS is established or not
     def test_delete_operation(self):
         sqlObj = sql_helper()
         deleteTask= sqlObj.run_query('Delete from Tasks where TaskID=1000')
-        task_details = sqlObj.run_query('select * from Tasks where Taskname = "Get milk and bread"')
+        task_details = sqlObj.run_query('select * from Tasks where TaskID = "1000"')
         task_details = str(task_details)
 
 if __name__ == "__main__":
